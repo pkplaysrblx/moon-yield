e974d7420f31bdfad7961f5380142ebb33b9618b
External Bypassed <neerurani740@gmail.com>
2024-11-21 18:00:25 +0000

    Update installer.lua

--- installer.lua
+++ installer.lua
@@ -1,2 +1,7 @@
 makefolder("moonyield")
-writefile("moonyield/script.lua","")
+writefile("moonyield/script.lua","loadstring(game:HttpGet('https://raw.githubusercontent.com/pkplaysrblx/moon-yield/refs/heads/main/script.lua'))()")
+local function notif(text)
+	game.StarterGui:SetCore("SendNotification", {Title = "Moon Yield Loader", Text = text})
+end
+
+notif("The loader has finished. Execute the script via. loadfile('moonyield/script.lua')()")
