3a984404e240e7dafab289d5b82c9ed6799bdd5a
External Bypassed <neerurani740@gmail.com>
2024-12-25 11:24:57 +0000

    yes

--- checker.lua
+++
@@ -1,9 +0,0 @@
-print("Executor: " .. identifyexecutor())
-print("Identity: " .. printidentity(""))
-print("GetHui(): " .. gethui())
-
-if hookfunction then
-print("Supported: Yes!")
-else
-print("Your executor (" .. identifyexecutor() .. ") is not supported! (missing hookfunction")
-end
